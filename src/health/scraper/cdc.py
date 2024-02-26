0a331b0d89eeba1f04805083f079a38d8570ffb7
Santiago Saldivar <ssaldivar1@fordham.edu>
2024-02-26 16:35:27 -0500

    adding the health topic to our database

---
+++ src/health/scraper/cdc.py
@@ -0,0 +1,77 @@
+from process import clean_items
+from database import WriteItems, ReadArticles
+from response import Response
+from notification import SendNotification
+import logging
+import xml.etree.ElementTree as ET
+from dotenv import load_dotenv
+load_dotenv()
+
+
+def main():
+    url = "https://tools.cdc.gov/podcasts/feed.asp?feedid=183"
+
+    table = 'cdc_newsroom'
+    topic = 'health'
+    link_variable_name = 'link'
+    notification_title = 'CDC News'
+    item_name = 'entry'
+
+
+    resp = Response(table, topic, url, link_variable_name, item_name)
+    xml_string, response = resp.get_soup()
+    data = []
+
+
+
+    """
+    Edit the XML based on your needs
+    """
+    for item in xml_string: 
+        print(item)
+        entry_data = {}
+        root = ET.fromstring(str(item))
+        for child in root:
+            if child.tag == 'link':
+                entry_data['link'] = child.attrib['href']
+            elif child.tag == 'category':
+                entry_data['category'] = child.attrib['term']
+            elif child.tag == 'summary':
+                entry_data['description'] = child.text.strip()
+            elif child.tag  == 'updated':
+                entry_data['pubDate'] = child.text
+            elif child.tag == 'id':
+                entry_data['guid'] = child.text
+            else:
+                entry_data[child.tag] = child.text.strip() if child.text else ''
+        data.append(entry_data)
+
+
+
+    items = []
+    for item in data:
+        scrapped = ReadArticles().check_item(table, item[link_variable_name])
+        if scrapped == False:
+            item = resp.log_item(item, response)
+            items.append(item)
+
+    
+    if len(items) > 0:
+        number_of_items = len(items)
+        print(number_of_items)
+        cleaned = clean_items(items)
+        print(cleaned)
+        WriteItems().process_item(cleaned, table, topic)
+        notify = SendNotification(topic)
+        recent = notify.get_recent_value(cleaned)
+        message = notify.message(cleaned, recent['title'])
+        notify.notification_push(notification_title, str(message))
+        
+        logging.info(f'The total items needed are: {number_of_items}')
+    else:
+        logging.info(f'No new items found for {table.title()}')
+
+
+
+if __name__ == '__main__':
+    main()
