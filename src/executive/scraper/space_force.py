23b75f1dba490922f064130063f99d80ebd63357
Santiago Saldivar <ssaldivar1@fordham.edu>
2024-04-19 13:58:33 -0400

    adding new space force version to the repo

--- src/executive/scraper/space_force.py
+++ src/executive/scraper/space_force.py
@@ -50,10 +50,10 @@ def main():
         number_of_items = len(items)
         cleaned = clean_items(items)
         print(cleaned)
-        # WriteItems().process_item(cleaned, table, topic)
-        recent = notify.get_recent_value(cleaned)
-        message = notify.message(cleaned, recent['title'])
-        notify.notification_push(topic,notification_title, str(message))
+        WriteItems().process_item(cleaned, table, topic)
+        # recent = notify.get_recent_value(cleaned)
+        # message = notify.message(cleaned, recent['title'])
+        # notify.notification_push(topic,notification_title, str(message))
         
         logging.info(f'The total items needed are: {number_of_items}')
     else:
