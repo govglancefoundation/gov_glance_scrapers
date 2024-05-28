8aaf84a5b61ec1062df11832bad89c13980d6115
Santiago Saldivar <ssaldivar1@fordham.edu>
2024-05-28 13:45:02 -0400

    adding puebla to the repo

---
+++ src/mexico/state_news/scraper/puebla.py
@@ -0,0 +1,70 @@
+from process import clean_items
+from database import WriteItems, ReadArticles
+from response import Response
+from notification import SendNotification
+from bs4 import BeautifulSoup
+import logging
+import xml.etree.ElementTree as ET
+import re 
+from dotenv import load_dotenv
+load_dotenv()
+
+
+def main():
+    url = 'https://www.puebla.gob.mx/index.php/noticias'          # url
+    table = 'Puebla'   
+    schema = 'mexico'                                                                 # State name
+    topic = 'state'                                                 # The topic of the scraper
+    link_variable_name = 'link'                                      # Whatever the link variable name might be
+    notification_title = 'Puebla State Updates'                    # Notification title
+    item_name = 'article'                                           # Make sure that you using the right item tag name
+    format = 'html.parser'
+    # notify = SendNotification()
+
+
+    resp = Response(table, topic, url, link_variable_name, item_name)
+    response = resp.request_content(verify=False)
+    soup = BeautifulSoup(response.content, format)
+    content = soup.find_all('div', {'class':'itemcat'})
+    data = []
+
+    """
+    Edit the XML based on your needs
+    - Skip the first index of the list since its just headers of the table
+    """
+    for item in content[:10]: 
+        item_dict = {}
+        item_dict['link'] = 'https://www.puebla.gob.mx' + item.find('a')['href']
+        item_dict['title'] = item.find('div', {'class':'itemcat-title'}).text.lstrip().rstrip()
+        item_dict['image'] = 'https://www.puebla.gob.mx' + item.find('img')['src']
+        item_dict['pubDate'] = item.find('div', {'class':'itemcat-created'}).text.lstrip().rstrip()
+        item_dict['description'] = item.find('p').text
+        data.append(item_dict)
+
+    print(data)
+
+    items = []
+    for item in data:
+        scrapped = ReadArticles(schema=schema).check_item(table, item[link_variable_name])
+        if scrapped == False:
+            item = resp.log_item(item, response)
+            items.append(item)
+
+    if len(items) > 0:
+        number_of_items = len(items)
+        print(number_of_items)
+        cleaned = clean_items(items)
+        print(cleaned)
+        WriteItems(schema=schema).process_item(cleaned, table, topic)
+    # # #     # recent = notify.get_recent_value(cleaned)
+    # # #     # message = notify.message(cleaned, recent['title'])
+    # # #     # notify.notification_push(topic,notification_title, str(message))
+        
+    #     logging.info(f'The total items needed for {table.title()} are: {number_of_items}')
+    # else:
+    #     logging.info(f'No new items found for {table.title()}')
+
+
+
+if __name__ == '__main__':
+    main()
