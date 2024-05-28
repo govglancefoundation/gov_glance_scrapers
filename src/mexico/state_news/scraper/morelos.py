424a5619cba5f10665710ccee7f582e4864555c8
Santiago Saldivar <ssaldivar1@fordham.edu>
2024-05-28 13:42:30 -0400

    adding morelos to the repo, dates are working

---
+++ src/mexico/state_news/scraper/morelos.py
@@ -0,0 +1,71 @@
+from process import clean_items
+from database import WriteItems, ReadArticles
+from response import Response
+from notification import SendNotification
+from bs4 import BeautifulSoup
+import logging
+import xml.etree.ElementTree as ET
+import re 
+from dotenv import load_dotenv
+load_dotenv()
+
+
+def main():
+    url = 'https://morelos.gob.mx/?q=boletines'   #url
+    table = 'Morelos'   
+    schema = 'mexico'                 # State name
+    topic = 'state'                                     # The topic of the scraper
+    link_variable_name = 'link'                         # Whatever the link variable name might be
+    notification_title = 'Morelos State Updates'       # Notification title
+    item_name = 'article'                               # Make sure that you using the right item tag name
+    format = 'html.parser'
+    # notify = SendNotification()
+    
+
+
+    resp = Response(table, topic, url, link_variable_name, item_name)
+    content , response = resp.request_render(tag_name ='div.col-lg-6')
+    data = []
+
+    """
+
+    Edit the XML based on your needs
+    """
+    for item in content: 
+        item_dict = {}
+
+
+        item_dict['link'] = list(item.find('a')[0].absolute_links)[0]
+        title = item.find('img')[0].attrs['title']
+        item_dict['title'] = title
+        item_dict['pubDate'] = item.find('div span.italica')[0].text.split('-')[-1].lstrip()
+        item_dict['description'] = ' '.join(''.join([content.text for content in item.find('p', first=False)]).split()).replace('• ','').replace(' Leer más','')
+        data.append(item_dict)
+
+    print(data)
+
+    items = []
+    for item in data:
+        scrapped = ReadArticles(schema=schema).check_item(table, item[link_variable_name])
+        if scrapped == False:
+            item = resp.log_item(item, response)
+            items.append(item)
+
+    if len(items) > 0:
+        number_of_items = len(items)
+        print(number_of_items)
+        cleaned = clean_items(items)
+        print(cleaned)
+        WriteItems(schema=schema).process_item(cleaned, table, topic)
+    # #     # recent = notify.get_recent_value(cleaned)
+    # #     # message = notify.message(cleaned, recent['title'])
+    # #     # notify.notification_push(topic,notification_title, str(message))
+        
+        logging.info(f'The total items needed for {table.title()} are: {number_of_items}')
+    else:
+        logging.info(f'No new items found for {table.title()}')
+
+
+
+if __name__ == '__main__':
+    main()
